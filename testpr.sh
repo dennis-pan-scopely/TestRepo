8e5f2c826695b3f582c801dd3215326d9a715aa1
Dennis Pan <dennis@scopely.com>
2018-03-26 18:15:15 -0700

    branch name

--- testpr.sh
+++ testpr.sh
@@ -2,5 +2,5 @@
 
 git checkout -b dennis/test
 git push -u origin dennis/test
-curl -u $USERNAME --key $KEY -d "{"title":"testTitle","head":"branch_1","base":"master","body":"123"}" https://api.github.com/repos/dennis-pan-scopely/TestRepo/pulls
+curl -u $USERNAME --key $KEY -d "{"title":"testTitle","head":"dennis/test","base":"master","body":"123"}" https://api.github.com/repos/dennis-pan-scopely/TestRepo/pulls
 
