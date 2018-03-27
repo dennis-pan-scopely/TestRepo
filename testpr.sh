32efdd930e8e8aa88128b8cd391391fddaed8743
Dennis Pan <dennis@scopely.com>
2018-03-26 18:13:38 -0700

    add test script

---
+++ testpr.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+git checkout -b dennis/test
+git push -u origin dennis/test
+curl -u $USERNAME --key $KEY -d "{"title":"testTitle","head":"branch_1","base":"master","body":"123"}" https://api.github.com/repos/dennis-pan-scopely/TestRepo/pulls
+
