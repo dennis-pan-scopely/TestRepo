c69d12abfe01da98e68bcd30ef5fcc2bb63d8b71
Dennis Pan <dennis@scopely.com>
2018-03-26 18:34:39 -0700

    debug

--- testpr.sh
+++ testpr.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-
+echo $USERNAME
+echo $KEY
 #git checkout -b dennis/test
 #git push -u origin dennis/test
 curl -u $USERNAME --key $KEY -d "{"title":"testTitle","head":"dennis/test1","base":"master","body":"123"}" https://api.github.com/repos/dennis-pan-scopely/TestRepo/pulls
